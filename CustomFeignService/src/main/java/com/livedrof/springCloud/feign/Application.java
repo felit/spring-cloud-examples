2d30e6ea84be93e92b709562f4adb6efae63f075
congshuanglong <congshuanglong@126.com>
2018-05-04 02:59:57 +0800

    添加EnableFeignClients后亦可用

--- CustomFeignService/src/main/java/com/livedrof/springCloud/feign/Application.java
+++ CustomFeignService/src/main/java/com/livedrof/springCloud/feign/Application.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableHystrix
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) {
