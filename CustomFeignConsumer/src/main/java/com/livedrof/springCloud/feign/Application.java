bc3a061f24b7ce658a77c49d2c4899df7f3a7865
congshuanglong <congshuanglong@126.com>
2018-05-04 00:20:04 +0800

    update

--- CustomFeignConsumer/src/main/java/com/livedrof/springCloud/feign/Application.java
+++ CustomFeignConsumer/src/main/java/com/livedrof/springCloud/feign/Application.java
@@ -2,12 +2,14 @@ package com.livedrof.springCloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
