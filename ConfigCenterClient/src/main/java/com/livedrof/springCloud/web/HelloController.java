d16b0c9e1c882b43cff42cf311840809e333f4d6
congshuanglong <congshuanglong@126.com>
2018-05-05 13:54:52 +0800

    配置中心客户端完成

--- ConfigCenterClient/src/main/java/com/livedrof/springCloud/web/HelloController.java
+++ ConfigCenterClient/src/main/java/com/livedrof/springCloud/web/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
-
+    @Value("${redis.host}")
     private String hello;
 
     @RequestMapping("/hello")
