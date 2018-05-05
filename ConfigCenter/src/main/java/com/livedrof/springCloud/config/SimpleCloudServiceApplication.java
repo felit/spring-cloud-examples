a55a60fbd60db426584f2957da4232fe0379ae27
congshuanglong <congshuanglong@126.com>
2018-05-05 13:47:57 +0800

    配置中心完成

--- ConfigCenter/src/main/java/com/livedrof/springCloud/config/SimpleCloudServiceApplication.java
+++ ConfigCenter/src/main/java/com/livedrof/springCloud/config/SimpleCloudServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleCloudServiceApplication {
 
     public static void main(String[] args) {
-        args = new String[1];
+//        args = new String[1];
 //        args[0] = "--spring.profiles.active=simple2";
 //        SpringApplication.run(SimpleCloudServiceApplication.class, args);
         SpringApplication.run(SimpleCloudServiceApplication.class);
